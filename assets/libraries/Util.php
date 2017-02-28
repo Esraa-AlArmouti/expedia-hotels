b382092461c6273dce7a117cf048bea62c768916
esraa_alarmouti <esraaalarmouti@gmail.com>
2017-02-28 12:57:16 -0800

    fixing conditions

--- assets/libraries/Util.php
+++ assets/libraries/Util.php
@@ -24,24 +24,43 @@ class Util
         $filtered_data = [];
         for($i = 0; $i < count($json_data); $i++){
             $lengthOfStay = Util::CalculateLengthOfStay($_POST['startDate'],$_POST['endDate']);
-            if($json_data[$i]->hotelUrgencyInfo->numberOfRoomsLeft > 0){
-                //array_push($filtered_data,$json_data[$i]);
-                //all result are not available in all hotels
-            }
             $startDate = Util::changeDateFormat($_POST['startDate']);
             $endDate = Util::changeDateFormat($_POST['endDate']);
             if($json_data[$i]->offerDateRange->lengthOfStay == $lengthOfStay &&
-                (isset($_POST['startDate']) &&!empty($_POST['startDate']) && $startDate ==  $json_data[$i]->hotelInfo->travelStartDate )&&
-                (isset($_POST['endDate']) &&!empty($_POST['endDate']) && $endDate ==  $json_data[$i]->hotelInfo->travelEndDate ) //&&
-                //(isset($_POST['Destination']) &&!empty($_POST['Destination']) && strpos($json_data[$i]->destination->longName ,$_POST['Destination']) != false)
+                Util::checkStartDate($startDate,$json_data[$i]) &&
+                Util::checkEndDate($endDate,$json_data[$i]) &&
+                Util::checkDestination($json_data[$i])
             ){
                 array_push($filtered_data,$json_data[$i]);
-                //all result are not available in all hotels
+            }elseif ($json_data[$i]->offerDateRange->lengthOfStay == $lengthOfStay ||
+                Util::checkStartDate($startDate,$json_data[$i]) ||
+                Util::checkEndDate($endDate,$json_data[$i]) ||
+                Util::checkDestination($json_data[$i])
+            ){
+                array_push($filtered_data,$json_data[$i]);
             }
 
         }
         return $filtered_data;
     }
-
+    public static function checkStartDate($startDate,$object){
+      if((isset($_POST['startDate']) &&!empty($_POST['startDate']) && $startDate ==  $object->hotelInfo->travelStartDate )){
+          return true;
+      }
+      return false;
+    }
+    public static function checkEndDate($endDate,$object){
+        if(isset($_POST['endDate']) &&!empty($_POST['endDate']) && $endDate ==  $object->hotelInfo->travelEndDate ){
+            return true;
+        }
+        return false;
+    }
+    public static function checkDestination($object){
+        //var_dump(strpos(strtolower($object->destination->longName) ,strtolower($_POST['Destination'])) !== false);die();
+        if(isset($_POST['Destination']) && !empty($_POST['Destination']) && strpos(strtolower($object->destination->longName) ,strtolower($_POST['Destination'])) !== false){
+            return true;
+        }
+        return false;
+    }
 }
 ?>
